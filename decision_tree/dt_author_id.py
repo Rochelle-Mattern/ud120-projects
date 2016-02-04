5b582587799d8750eea8bbf926a275eac3691e34
Rochelle Mattern <rochelle.mattern.92@gmail.com>
2016-02-03 22:03:59 -0800

    complete code and access accuracy
    sklearn decision tree with min samples split = 40

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -1,31 +1,34 @@
-#!/usr/bin/python
-
-""" 
-    This is the code to accompany the Lesson 3 (decision tree) mini-project.
-
-    Use a Decision Tree to identify emails from the Enron corpus by author:    
-    Sara has label 0
-    Chris has label 1
-"""
-    
-import sys
-from time import time
-sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
-
+print (len(features_train[0]))
 
 
 
 #########################################################
 ### your code goes here ###
+###clf1 = tree.DecisionTreeClassifier(min_samples_split=2)
+clf2 = tree.DecisionTreeClassifier(min_samples_split=40)
 
+###clf1.fit(features_train, labels_train)
+clf2.fit(features_train, labels_train)
+
+###pred1 = clf1.predict(features_test)
+pred2 = clf2.predict(features_test)
+
+
+###acc_min_samples_split_2 = accuracy_score(pred1, labels_test)
+acc = accuracy_score(pred2, labels_test)
+print acc
 
 #########################################################
+
+
 
 
