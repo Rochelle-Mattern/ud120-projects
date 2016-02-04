684c17de088d315cdd39ef233d23ccbd73537659
Rochelle Mattern <rochelle.mattern.92@gmail.com>
2016-02-03 22:08:22 -0800

    SVM code completed and accuracy scored
    sklearn SVM mini-project

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -12,7 +12,8 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -23,7 +24,20 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 #########################################################
-### your code goes here ###
+clf = SVC(kernel="rbf", C= 10000.0)
+t0 = time()
+###features_train = features_train[:len(features_train)/100]
+###labels_train = labels_train[:len(labels_train)/100]
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+t1 = time ()
+pred = clf.predict(features_test)
+print "predicting time:", round(time()-t1, 3), "s"
+score = accuracy_score(pred, labels_test)
+count = 0
+answer = pred[50]
+print(answer)
+print (sum(pred))
 
 #########################################################
 
